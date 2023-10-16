1200730390256398d20218871ec52c555812ab83
Marcelo <marcelo.mgr@policiacientifica.sp.gov.br>
2023-10-16 20:56:35 -0300

    Criação do projeto MVC para gestão de pessoas.

---
+++ PersonService/Consumers/MVC/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    // Adicione um botão de alternância para recolher/expandir o menu em telas menores
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
