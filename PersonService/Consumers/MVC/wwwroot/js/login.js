fa25f35e751419102388263c989ae7082f662eff
Marcelo <marcelo.mgr@policiacientifica.sp.gov.br>
2023-11-13 19:52:17 -0300

    Ajuste de responsividade, interatividade da pagina de login

---
+++ PersonService/Consumers/MVC/wwwroot/js/login.js
@@ -0,0 +1,7 @@
+﻿
+$(document).ready(function () {
+    function redirectToHome() {
+        // Redirecionamento para a página Home/Index
+        window.location.href = '@Url.Action("Index", "Home")';
+    }
+})
